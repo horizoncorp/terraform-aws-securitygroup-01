9b1fa610ed252c3b48ed421b18ad4b96ff5798d6
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 13:45:37 +0530

    remediate output

--- main.tf
+++ main.tf
@@ -1,43 +1,16 @@
-locals {
-  create_sg = var.security_group_id == null ? 0 : 1
-  not_create_sg = var.security_group_id == null ? 1 : 0
-}
-
-resource "aws_security_group" "sg" {
-  count  = local.create_sg
-  name   = var.name
-  vpc_id = var.vpc_id
-}
-
-resource "aws_security_group_rule" "default_rule" {
-  count             = local.create_sg
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.sg[0].id
-}
-
-resource "aws_security_group_rule" "sg_rule" {
-  count             = local.not_create_sg
-  type              = var.sg_rule_type
-  from_port         = var.from_port
-  to_port           = var.to_port
-  protocol          = var.protocol
-  cidr_blocks       = var.cidr_blocks
-  security_group_id = var.security_group_id
-}
-
+# locals {
+#   create_sg = var.security_group_id == null ? 0 : 1
+#   not_create_sg = var.security_group_id == null ? 1 : 0
+# }
 
 # resource "aws_security_group" "sg" {
-#   count  = var.security_group_id == null ? 0 : 1
+#   count  = local.create_sg
 #   name   = var.name
 #   vpc_id = var.vpc_id
 # }
 
 # resource "aws_security_group_rule" "default_rule" {
-#   count             = var.security_group_id == null ? 0 : 1
+#   count             = local.create_sg
 #   type              = "egress"
 #   from_port         = 0
 #   to_port           = 0
@@ -47,7 +20,7 @@ resource "aws_security_group_rule" "sg_rule" {
 # }
 
 # resource "aws_security_group_rule" "sg_rule" {
-#   count             = var.security_group_id == null ? 1 : 0
+#   count             = local.not_create_sg
 #   type              = var.sg_rule_type
 #   from_port         = var.from_port
 #   to_port           = var.to_port
@@ -55,3 +28,30 @@ resource "aws_security_group_rule" "sg_rule" {
 #   cidr_blocks       = var.cidr_blocks
 #   security_group_id = var.security_group_id
 # }
+
+
+resource "aws_security_group" "sg" {
+  count  = var.security_group_id == null ? 0 : 1
+  name   = var.name
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "default_rule" {
+  count             = var.security_group_id == null ? 0 : 1
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.sg[0].id
+}
+
+resource "aws_security_group_rule" "sg_rule" {
+  count             = var.security_group_id == null ? 1 : 0
+  type              = var.sg_rule_type
+  from_port         = var.from_port
+  to_port           = var.to_port
+  protocol          = var.protocol
+  cidr_blocks       = var.cidr_blocks
+  security_group_id = var.security_group_id
+}
