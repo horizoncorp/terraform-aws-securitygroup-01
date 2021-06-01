0638bc91f049bf7df229227fa2081fbe45a30d1b
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 15:24:01 +0530

    remediate output

--- output.tf
+++ output.tf
@@ -1,4 +1,13 @@
+# output "sg_id" {
+#   description = "SG ID"
+#   value       = var.security_group_id == "" ? aws_security_group.sg[0].id : var.security_group_id 
+# }
+
+locals {
+  sg_value = var.security_group_id == "" ? aws_security_group.sg[0].id : var.security_group_id 
+}
+
 output "sg_id" {
   description = "SG ID"
-  value       = var.security_group_id == "" ? aws_security_group.sg[0].id : var.security_group_id 
+  value       = local.sg_value
 }
