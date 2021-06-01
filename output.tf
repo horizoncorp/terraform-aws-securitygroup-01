1910edc1e358bcf2f6bb092d6ee96eaf7b0d6dd2
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 11:43:36 +0530

    customizing output file

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "sg_id" {
   description = "SG ID"
-  value       = length(var.security_group_id) == null ? true : aws_security_group.sg.id
+  value       = length(var.security_group_id) == null ? true : aws_security_group.sg[0].id
 }
