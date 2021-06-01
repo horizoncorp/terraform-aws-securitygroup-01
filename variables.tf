e4ace852fa44960d34a6a3754278dd27c2a02120
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 15:51:42 +0530

    remediate output

--- variables.tf
+++ variables.tf
@@ -1,24 +1,6 @@
 variable "name" {
     default = ""
 }
-variable "security_group_id" {
-    default = ""
-}
 variable "vpc_id" {
     default = ""
-}
-variable "sg_rule_type" {
-    default = "egress"
-}
-variable "from_port" {
-    default = 0
-}
-variable "to_port" {
-    default = 0
-}
-variable "protocol" {
-    default = ""
-}
-variable "cidr_blocks" {
-    default = []
 }
