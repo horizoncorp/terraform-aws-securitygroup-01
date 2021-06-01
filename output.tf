0c1413d48fdf7e2fe600bde2892e640f63817662
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 12:11:23 +0530

    customizing count variable

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "sg_id" {
   description = "SG ID"
-  value       = var.security_group_id == null ? aws_security_group.sg[0].id : false
+  value       = var.security_group_id == null ? aws_security_group.sg[0].id : var.security_group_id
 }
