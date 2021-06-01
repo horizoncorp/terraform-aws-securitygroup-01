31555e6d6f136893418454b988b0f1967eb69bca
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 12:26:08 +0530

    remediate output

--- modules/allow-ssh-22/main.tf
+++ modules/allow-ssh-22/main.tf
@@ -2,7 +2,7 @@ module "ssh_22" {
   source            = "../../"
   name              = var.name
   security_group_id = var.security_group_id
-  type              = "ingress"
+  sg_rule_type      = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
