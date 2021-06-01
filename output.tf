1663e9d289cbdf78af70d81a8ef36eb21321320c
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 11:40:43 +0530

    File resource name conflict

--- output.tf
+++ output.tf
@@ -1,5 +1,4 @@
 output "sg_id" {
-  count  = length(var.security_group_id) ? 0 : 1
   description = "SG ID"
-  value       = aws_security_group.sg.id
+  value       = length(var.security_group_id) == null ? true : aws_security_group.sg.id
 }
