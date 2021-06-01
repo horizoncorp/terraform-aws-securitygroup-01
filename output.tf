bfceb627a733a7bafb3cc96b4458c8e82a8da0d1
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 11:33:12 +0530

    Update output

--- output.tf
+++ output.tf
@@ -1,10 +1,5 @@
-output "bastion_private_key" {
-  description = "Private key generated for bastion"
-  sensitive   = true
-  value       = tls_private_key.bastion_private_ssh_key.private_key_pem
-}
-
-output "bastion_public_ip" {
-  description = "Public IP of bastion"
-  value       = aws_instance.bastion.public_ip
+output "sg_id" {
+  count  = length(var.security_group_id) ? 0 : 1
+  description = "SG ID"
+  value       = aws_security_group.sg.id
 }
