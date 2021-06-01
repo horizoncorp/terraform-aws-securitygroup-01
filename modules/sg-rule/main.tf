1f4198f51c5661bd2a7e57b6b13ea915ac843e17
maneeshkumarkg <maneeshkumarkg@outlook.com>
2021-06-01 16:00:45 +0530

    remediate output

--- modules/sg-rule/main.tf
+++ modules/sg-rule/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_security_group_rule" "sg_rule" {
-  type              = var.sg_rule_type
+  sg_rule_type      = var.sg_rule_type
   from_port         = var.from_port
   to_port           = var.to_port
   protocol          = var.protocol
